4fcba920ab4e27e6552892047f7346f0dda6c0fd
Beatriz Murase <biamurase@gmail.com>
2020-06-10 17:24:23 -0300

    feat: add navigation to profile

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
+import { useNavigation } from '@react-navigation/native';
 import { useAuth } from '../../hooks/auth';
 
-import { Container, Header, HeaderTitle, UserName } from './styles';
+import {
+  Container,
+  Header,
+  HeaderTitle,
+  UserName,
+  ProfileButton,
+  UserAvatar,
+} from './styles';
 
 const Dashboard: React.FC = () => {
   const { user } = useAuth();
+  const { navigate } = useNavigation();
+
+  const navigateToProfile = useCallback(() => {
+    navigate('Profile');
+  }, [navigate]);
 
   return (
     <Container>
       <Header>
         <HeaderTitle>
           Bem vinde, {'\n'}
-          <UserName>Beatriz Murase</UserName>
+          <UserName>{user.name}</UserName>
         </HeaderTitle>
+
+        <ProfileButton onPress={navigateToProfile}>
+          <UserAvatar source={{ uri: user.avatar_url }} />
+        </ProfileButton>
       </Header>
     </Container>
   );
