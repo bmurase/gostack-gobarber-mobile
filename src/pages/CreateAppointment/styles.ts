4d6e3022b1213e68e94cd87c80d60c4b46acdf37
Beatriz Murase <biamurase@gmail.com>
2020-06-12 00:06:20 -0300

    style: schedules

--- src/pages/CreateAppointment/styles.ts
+++ src/pages/CreateAppointment/styles.ts
@@ -43,6 +43,8 @@ export const UserAvatar = styled.Image`
   margin-left: auto;
 `;
 
+export const Content = styled.ScrollView``;
+
 export const ProvidersListContainer = styled.View`
   height: 112px;
 `;
@@ -95,4 +97,38 @@ export const OpenDatePickerButtonText = styled.Text`
   font-family: 'RobotoSlab-Medium';
   font-size: 16px;
   color: #232129;
+`;
+
+export const Schedule = styled.View`
+  padding: 24px 0 16px;
+`;
+
+export const Section = styled.View`
+  margin-bottom: 24px;
+`;
+
+export const SectionTitle = styled.Text`
+  font-size: 18px;
+  color: #999591;
+  font-family: 'RobotoSlab-Regular';
+  margin: 0 24px 12px;
+`;
+
+export const SectionContent = styled.ScrollView.attrs({
+  contentContainerStyle: { paddingHorizontal: 24 },
+  horizontal: true,
+  showsHorizontalScrollIndicator: false,
+})``;
+
+export const Hour = styled.View`
+  padding: 12px;
+  background: #3e3b47;
+  border-radius: 10px;
+  margin-right: 8px;
+`;
+
+export const HourText = styled.Text`
+  color: #f4ede8;
+  font-family: 'RobotoSlab-Regular';
+  font-size: 16px;
 `;
